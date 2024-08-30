a911d0c0d42bf66a0e252c35c82272583a1298dc
jmoletteire <jackmole@outlook.com>
2024-08-30 12:49:25 -0500

    updated multi-team tiebreaker logic

--- backend/splash_data/splash_nba/lib/teams/standings.py
+++ backend/splash_data/splash_nba/lib/teams/standings.py
@@ -1,45 +1,10 @@
+from itertools import groupby
+
 from nba_api.stats.endpoints import leaguestandings, leaguedashteamstats
 from pymongo import MongoClient
 from splash_nba.util.env import uri, k_current_season
 import logging
 from collections import defaultdict
-
-
-eastConfTeamIds = [
-    1610612737,
-    1610612738,
-    1610612739,
-    1610612741,
-    1610612748,
-    1610612749,
-    1610612751,
-    1610612752,
-    1610612753,
-    1610612754,
-    1610612755,
-    1610612761,
-    1610612764,
-    1610612765,
-    1610612766
-]
-
-westConfTeamIds = [
-    1610612740,
-    1610612742,
-    1610612743,
-    1610612744,
-    1610612745,
-    1610612746,
-    1610612747,
-    1610612750,
-    1610612756,
-    1610612757,
-    1610612758,
-    1610612759,
-    1610612760,
-    1610612762,
-    1610612763,
-]
 
 
 def determine_tiebreakers(season, standings):
@@ -58,7 +23,7 @@ def determine_tiebreakers(season, standings):
                     break_tie_two_teams(season, teams, 'DivisionRank', standings)
                 else:
                     # Handle tiebreaker for more than two teams
-                    break_tie_multiple_teams(season, teams, 'DivisionRank', standings)
+                    break_tie_multiple_teams(season, teams, 'DivisionRank', standings, 0)
             else:
                 logging.info(f"No tie for {rank} in {division} Division")
 
@@ -77,7 +42,7 @@ def determine_tiebreakers(season, standings):
                     break_tie_two_teams(season, teams, 'PlayoffRank', standings)
                 else:
                     # Handle tiebreaker for more than two teams
-                    break_tie_multiple_teams(season, teams, 'PlayoffRank', standings)
+                    break_tie_multiple_teams(season, teams, 'PlayoffRank', standings, 0)
             else:
                 logging.info(f"No tie for {conference} {rank}-seed")
 
@@ -149,8 +114,6 @@ def break_tie_two_teams(season, teams, rank, standings):
             team1_pct = team1_vs_team2[0]['W_PCT']
             team2_pct = 1 - team1_pct
 
-        logging.info(team1_pct)
-        logging.info(team2_pct)
         if team1_pct > team2_pct:
             logging.info(f"{team1['TeamCity']} wins the tiebreaker against {team2['TeamCity']} based on head-to-head.")
             team2[rank] += 1
@@ -166,7 +129,7 @@ def break_tie_two_teams(season, teams, rank, standings):
                 logging.info(f"{team2['TeamCity']} wins the tiebreaker against {team1['TeamCity']} based on division winner.")
                 team1[rank] += 1
             else:
-                # (3) Better winning percentage against teams in own division (only if tied teams are in the same division).
+                # (3) Better winning percentage against teams in own division (only if teams are in the same division).
                 if team1['Division'] == team2['Division'] and team1['DivisionRecord'] > team2['DivisionRecord']:
                     logging.info(f"{team1['TeamCity']} wins the tiebreaker against {team2['TeamCity']} based on division record.")
                     team2[rank] += 1
@@ -211,7 +174,6 @@ def break_tie_two_teams(season, teams, rank, standings):
                                         logging.error(
                                             f"Error fetching win percentage for {team['TeamCity']} against {opponent['TeamCity']}: {e}")
 
-                            # Average win percentage across all tied teams
                             try:
                                 total_win_pct = total_wins / total_games
                             except ZeroDivisionError:
@@ -248,7 +210,6 @@ def break_tie_two_teams(season, teams, rank, standings):
                                             logging.error(
                                                 f"Error fetching win percentage for {team['TeamCity']} against {opponent['TeamCity']}: {e}")
 
-                                # Average win percentage across all tied teams
                                 try:
                                     total_win_pct = total_wins / total_games
                                 except ZeroDivisionError:
@@ -276,69 +237,115 @@ def break_tie_two_teams(season, teams, rank, standings):
                                         logging.info("The tiebreaker couldn't be determined with the given criteria.")
 
 
+def break_tie_multiple_teams(season, teams, rank, standings, step):
+    def sort_and_group_teams(teams, key):
+        teams.sort(key=key, reverse=True)
+        grouped_teams = []
+        for key_value, group in groupby(teams, key=key):
+            grouped_teams.append(list(group))
+        return grouped_teams
 
-def break_tie_multiple_teams(season, teams, rank, standings):
-    # (1) Division winner
-    if rank == 'PlayoffRank':
-        division_winners = [team for team in teams if team['DivisionRank'] == 1]
-        non_division_winners = []
-        if division_winners:
-            logging.info(f"Division winners found in tiebreaker group: {[team['TeamCity'] for team in division_winners]}")
-            for team in teams:
-                if team not in division_winners:
-                    team['PlayoffRank'] += len(division_winners)
-                    non_division_winners.append(team)
-            if len(non_division_winners) > 0:
-                if len(non_division_winners) == 2:
-                    break_tie_two_teams(season, non_division_winners, rank, standings)
-                else:
-                    break_tie_multiple_teams(season, non_division_winners, rank, standings)
-            teams = division_winners
+    original_rank = teams[0][rank]  # Assume all teams start with the same rank
 
-    if len(teams) == 2:
-        break_tie_two_teams(season, teams, rank, standings)
+    def update_team_ranks(grouped_teams, starting_rank, step):
+        current_rank = starting_rank
+        for group in grouped_teams:
+            if len(group) == 1:
+                group[0][rank] = current_rank
+                logging.info(f"{group[0]['TeamCity']} assigned rank {current_rank} based on criteria {step}.")
+            elif len(group) == 2:
+                group[0][rank] = current_rank
+                group[1][rank] = current_rank
+                logging.info(f"{group[0]['TeamCity']} and {group[1]['TeamCity']} assigned ranks {current_rank}.")
+                break_tie_two_teams(season, group, rank, standings)
+            else:
+                step += 1
+                break_tie_multiple_teams(season, group, rank, standings, step)
+            current_rank += len(group)
+
+    # Step 0: Better overall winning percentage
+    if step == 0:
+        grouped_teams = sort_and_group_teams(teams, lambda team: team['WinPCT'])
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 1: Division winner
+    if step == 1:
+        grouped_teams = sort_and_group_teams(teams, lambda team: 1 if team['DivisionRank'] == 1 else 0)
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 2: Better winning percentage in games against each other
+    if step == 2:
+        grouped_teams = sort_and_group_teams(teams, lambda team: calculate_head_to_head_pct(team, teams, season))
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 3: Better winning percentage against teams in own division
+    if step == 3:
+        grouped_teams = sort_and_group_teams(teams, lambda team: team['DivisionRecord'])
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 4: Better winning percentage against teams in own conference
+    if step == 4:
+        grouped_teams = sort_and_group_teams(teams, lambda team: team['ConferenceRecord'])
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 5: Better winning percentage against playoff teams in own conference
+    if step == 5:
+        grouped_teams = sort_and_group_teams(teams, lambda team: calculate_playoff_win_pct(team, standings, season, own_conference=True))
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 6: Better winning percentage against playoff teams in opposite conference
+    if step == 6:
+        grouped_teams = sort_and_group_teams(teams, lambda team: calculate_playoff_win_pct(team, standings, season, own_conference=False))
+        update_team_ranks(grouped_teams, original_rank, step)
+
+    # Step 7: Better net points, all games
+    if step == 7:
+        grouped_teams = sort_and_group_teams(teams, lambda team: team['DiffPointsPG'] * (team['WINS'] + team['LOSSES']))
+        update_team_ranks(grouped_teams, original_rank, step)
+
+
+def calculate_head_to_head_pct(team, teams, season):
+    win_count = 0
+    game_count = 0
+    for opponent in teams:
+        if team['TeamID'] != opponent['TeamID']:
+            team_vs_opp = leaguedashteamstats.LeagueDashTeamStats(
+                season=season,
+                team_id_nullable=team['TeamID'],
+                opponent_team_id=opponent['TeamID']
+            ).get_normalized_dict()['LeagueDashTeamStats']
+
+            if team_vs_opp:
+                win_count += team_vs_opp[0]['W']
+                game_count += team_vs_opp[0]['GP']
+
+    return win_count / game_count if game_count > 0 else 0.0
+
+
+def calculate_playoff_win_pct(team, standings, season, own_conference=True):
+    if season < '2019-20':
+        eligible_teams = [t['TeamID'] for t in standings if t['Conference'] == team['Conference'] and t['PlayoffRank'] <= 10]
     else:
-        # (2) Better winning percentage in all games among the tied teams.
-        win_pcts = {}
+        eligible_teams = [t['TeamID'] for t in standings if t['Conference'] == team['Conference'] and t['PlayoffRank'] <= 8]
 
-        for team in teams:
-            total_games = 0
-            total_wins = 0
-            for opponent in teams:
-                if team != opponent:
-                    team1_id = team['TeamID']
-                    team2_id = opponent['TeamID']
+    if not own_conference:
+        eligible_teams = [t['TeamID'] for t in standings if t['Conference'] != team['Conference']]
 
-                    try:
-                        team_vs_opp = leaguedashteamstats.LeagueDashTeamStats(
-                            season=season,
-                            team_id_nullable=team1_id,
-                            opponent_team_id=team2_id
-                        ).get_normalized_dict()['LeagueDashTeamStats']
+    win_count = 0
+    game_count = 0
+    for opponent in eligible_teams:
+        if team['TeamID'] != opponent:
+            team_vs_opp = leaguedashteamstats.LeagueDashTeamStats(
+                season=season,
+                team_id_nullable=team['TeamID'],
+                opponent_team_id=opponent
+            ).get_normalized_dict()['LeagueDashTeamStats']
 
-                        if team_vs_opp:
-                            total_games += team_vs_opp[0]['GP']
-                            total_wins += team_vs_opp[0]['W']
-                    except Exception as e:
-                        logging.error(
-                            f"Error fetching win percentage for {team['TeamCity']} against {opponent['TeamCity']}: {e}")
+            if team_vs_opp:
+                win_count += team_vs_opp[0]['W']
+                game_count += team_vs_opp[0]['GP']
 
-            # Average win percentage across all tied teams
-            try:
-                total_win_pct = total_wins / total_games
-            except ZeroDivisionError:
-                total_win_pct = 0.0
-
-            win_pcts[team['TeamID']] = total_win_pct
-
-        # Sort the teams by their average winning percentage in descending order
-        sorted_teams = sorted(teams, key=lambda x: win_pcts[x['TeamID']], reverse=True)
-
-        # Adjust rank based on the sorted order
-        base_rank = sorted_teams[0][rank]
-        for i, team in enumerate(sorted_teams):
-            team[rank] = base_rank + i
-            logging.info(f"{team['TeamCity']} assigned rank {team[rank]} based on tiebreaker.")
+    return win_count / game_count if game_count > 0 else 0.0
 
 
 def update_current_standings():
