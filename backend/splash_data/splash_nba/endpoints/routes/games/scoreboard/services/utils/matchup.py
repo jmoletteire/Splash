096f51bb3892adda071c9bf83187954c5c302275
jmoletteire <jackmole@outlook.com>
2025-02-24 16:53:41 -0600

    try adding expected lineups for games not yet started

--- backend/splash_data/splash_nba/endpoints/routes/games/scoreboard/services/utils/matchup.py
+++ backend/splash_data/splash_nba/endpoints/routes/games/scoreboard/services/utils/matchup.py
@@ -1,3 +1,30 @@
+import sys
+
+env_path = "/home/ubuntu"
+if env_path not in sys.path:
+    sys.path.insert(0, env_path)  # Add /home/ubuntu to the module search path
+try:
+    from mongo_connect import get_mongo_collection
+except ImportError:
+    raise ImportError("mongo_connect.py could not be found locally or at /home/ubuntu.")
+
+
+def expected_lineup_data(team_id):
+    team = get_mongo_collection('teams').find_one({'TEAM_ID': team_id}, {'LAST_STARTING_LINEUP': 1})
+    last_lineup = team.get('LAST_STARTING_LINEUP', [])
+    lineup_final = []
+
+    for player in last_lineup:
+        name = player["name"].split(" ")
+        lineup_final.append({
+            "personId": player["PLAYER_ID"] if "PLAYER_ID" in player else None,
+            "name": f"{name[0][0]}. {name[-1]}" if name is not None else None,
+            "position": player["POSITION"] if "POSITION" in player else None,
+        })
+
+    return lineup_final
+
+
 def lineup_data(player):
     return {
         "personId": str(player["personId"]) if "personId" in player else None,
@@ -31,11 +58,26 @@ def matchup_details(summary, boxscore):
             lineups["home"] = [lineup_data(player) for player in boxscore["homeTeam"]["players"] if player["starter"] == "1"]
             order = [4, 3, 0, 2, 1]  # PG, SG, SF, C, PF
             lineups["home"] = [lineups["home"][i] for i in order]
+    else:
+        try:
+            lineups["home"] = expected_lineup_data(summary["GameSummary"][0]["HOME_TEAM_ID"])
+            order = [4, 3, 0, 2, 1]  # PG, SG, SF, C, PF
+            lineups["home"] = [lineups["home"][i] for i in order]
+        except Exception:
+            lineups["home"] = []
+
     if "awayTeam" in boxscore:
         if "players" in boxscore["awayTeam"]:
             lineups["away"] = [lineup_data(player) for player in boxscore["awayTeam"]["players"] if player["starter"] == "1"]
             order = [0, 2, 1, 4, 3]  # SF, C, PF, PG, SG
             lineups["away"] = [lineups["away"][i] for i in order]
+    else:
+        try:
+            lineups["away"] = expected_lineup_data(summary["GameSummary"][0]["VISITOR_TEAM_ID"])
+            order = [0, 2, 1, 4, 3]  # SF, C, PF, PG, SG
+            lineups["away"] = [lineups["away"][i] for i in order]
+        except Exception:
+            lineups["away"] = []
 
     # Inactive
     if "InactivePlayers" in summary:
