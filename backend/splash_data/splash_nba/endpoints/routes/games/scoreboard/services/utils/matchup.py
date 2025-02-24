6c453d2ea9b8baa905e69a07ed4fadc4fccaa3a3
jmoletteire <jackmole@outlook.com>
2025-02-24 17:21:07 -0600

    fix player name key to NAME

--- backend/splash_data/splash_nba/endpoints/routes/games/scoreboard/services/utils/matchup.py
+++ backend/splash_data/splash_nba/endpoints/routes/games/scoreboard/services/utils/matchup.py
@@ -22,7 +22,7 @@ def expected_lineup_data(team_id):
     lineup_final = []
 
     for player in last_lineup:
-        name = player["name"].split(" ")
+        name = player["NAME"].split(" ")
         lineup_final.append({
             "personId": player["PLAYER_ID"] if "PLAYER_ID" in player else None,
             "name": f"{name[0][0]}. {name[-1]}" if name is not None else None,
